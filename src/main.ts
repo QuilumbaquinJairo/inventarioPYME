6a7cc1c7ea2b8b3305977c9ff8bbd3e38354c056
Christopher <christho987@hotmail.com>
2025-02-27 07:36:04 -0500

    Feat: edit cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Permite solicitudes desde este origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Permite enviar cookies o encabezados de autenticación
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
